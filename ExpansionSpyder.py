a686194b942ec88cb1b1b385b31fa68595d5acb6
sebastian lopez ferre <sebas_ferre@hotmail.com>
2019-07-20 20:16:33 +0200

    Spyder v0.1

--- ExpansionSpyder.py
+++ ExpansionSpyder.py
@@ -13,48 +13,49 @@ logging.critical('---- Start of the program ---- ')
 year = [2016,2017,2018,2019] # La hemeroteca antes de 2016 tiene otro formato, tengo que haccer una araña diferentes para años antes del 2016
 month = [1,2,3,4,5,6,7,8,9,10,11,12]
 day = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31]
+listaweb = []
 
-def get_url_list (maxyears, maxmonths, maxdays):
+def create_url(day, month, year):
 
-    url2 = r"https://www.expansion.com/hemeroteca/"
-    url_list = []
+    url = r"https://www.expansion.com/hemeroteca/"
 
-    for i in range(0, maxyears):
-        url2 = url2 + str(year[i])
-        for j in range(0, maxmonths):
-            if j <= 9:
-                url2 = url2 + '/0' + str(month[j])
-            else:
-                url2 = url2 + '/' + str(month[j])
-            for k in range(0, maxdays):
-                if k <= 9:
-                    url2 = url2 + '/0' + str(day[k])
-                else:
-                    url2 = url2 + '/' + str(day[k])
-                url_list.append(url2)
-                url2 = url2[:-3]
-            url2 = url2[:-3]
-        url2 = url2[:-4]
+    #Format month and day so if it is less than 10, put a zero before
+    if month < 10:
+        month = "/0" + str(month)
+    else:
+        month = "/" + str(month)
 
-    return(url_list)
+    if day < 10:
+        day = "/0" + str(day)
+    else:
+        day = "/" + str(day)
 
-# listaweb = get_url_expansion(2,11,1)
-# print(listaweb)
+    url2 = url + str(year) + month + day
+    return url2
 
-# driver = webdriver.Chrome()
-#
-# for i in range(0, len(listaweb)):
-#     driver.get(listaweb[i])
-#     lista_titulares = driver.find_elements_by_class_name('noticia')
-#     print('\n\n' + " - LOS TITULARES DEL " + str(listaweb[i] + '\n\n'))
-#
-#     for j in range(0, 4):
-#         titular = lista_titulares[j].text
-#         logging.critical(' [+] ' + titular[:100])
-#
-#     lista_titulares.clear()
-#     sleep(2)
-#
-# driver.close()
-#
-# logging.CRITICAL(' ---- End of the program ---- ')
+for i in range(2):
+    listaweb.append(create_url(i,month[8], year[2])) #Primeros dos dias de Septiembre de 2018
+
+
+driver = webdriver.Chrome()
+
+for i in range(0, len(listaweb)):
+
+    dia = listaweb[i][-2:]
+    mes = listaweb[i][-5 : -3]
+    ano = listaweb[i][-10 : -6]
+
+    driver.get(listaweb[i])
+    lista_titulares = driver.find_elements_by_class_name('noticia')
+    print('\n\n' + " [+] Los titulares del día " + str(dia) + " de " + str(mes) + " de " + str(ano) + '\n\n')
+
+    for j in range(0, len(lista_titulares)):
+        titular = lista_titulares[j].text
+        logging.critical(' [!] ' + titular)
+
+    lista_titulares.clear()
+    sleep(2)
+
+driver.close()
+
+logging.CRITICAL(' ---- End of the program ---- ')
